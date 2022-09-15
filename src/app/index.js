a3b491438852337f44b13ec738f1dad205c850bf
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-09-15 22:31:12 +0500

    Добавление webpack

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,13 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+
 export const App = () => {
+    useEffect(() => {
+        const fetchAx = async () => {
+            const data = await axios.get('/api/users');
+            console.log(data);
+        }
+        fetchAx();
+    }, [])
     return <h1>Hello</h1>
 }
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+}
